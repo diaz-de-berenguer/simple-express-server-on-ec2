60720b973549fa388a2e404e9ea46737d5d2f698
Diego Diaz de Berenguer <d.diazdeberenguer@gmail.com>
2018-02-19 20:04:03 -0600

    Add an HTML page

--- index.js
+++ index.js
@@ -1,8 +1,5 @@
 const express = require('express')
 const app = express()
 
-app.get('/', (req, res) => {
-	res.send('Hola Mundo!')
-})
-
+app.use(express.static('public'))
 app.listen(3000, () => console.log("Feel the rhythm\nFeel the rhyme\nGet on up\nIt's serving time!\n\nServer Running!"))
